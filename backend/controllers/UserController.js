1c224e2f40e53435b082a55fb767f0b55776142f
Inhaz Fahim <inhazfahimdev@gmail.com>
2025-03-21 19:59:51 +0530

    Login API completed

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -11,6 +11,16 @@ class UserController{
             res.status(400).json({error:error.message})
         }
     }
+
+    static async userLogin (req,res) {
+        try{
+            const response = await userService.userLogin(req.body);
+            return res.status(200).json({response: response.Message});
+        }
+        catch(error){
+            res.status(400).json({error:error.message})
+        }
+    }
 }
 
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/register", UserController.userRegistration);
+router.post("/login", UserController.userLogin);
 
 module.exports = router;
 
